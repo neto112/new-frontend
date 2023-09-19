30a793123590058ca46b56e3a2bf3ff0aed75cd4
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-09-19 09:15:38 -0300

    Hangman

---
+++ src/views/Games/TicTacToe/Hangman/PopupView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="finalMessage" class="popup-container" id="popup-container">
+    <div class="popup">
+      <h2>{{ finalMessage }}</h2>
+      <h3 v-show="status === 'lose'">...the word was: {{ word }}</h3>
+      <button @click="reset">Play Again</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  status: { type: String, default: "" },
+  word: { type: String, default: "" },
+});
+
+const emits = defineEmits(["reset"]);
+
+const finalMessage = computed(() => {
+  if (props.status === "win") return "Congratulations! You won! 😃";
+  if (props.status === "lose") return "Unfortunately you lost. 😕";
+  return "";
+});
+
+const reset = () => emits("reset");
+</script>
---
+++ src/views/Games/TicTacToe/Hangman/WordView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="word">
+    <span class="letter" v-for="(letter, i) in letters" :key="i">
+      {{ correctLetters.includes(letter) ? letter : "" }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps<{
+  letters: { type: Array<string>; required: true };
+  correctLetters: { type: Array<string>; default: () => [] };
+}>();
+</script>
